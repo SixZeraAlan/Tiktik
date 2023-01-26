8355661a1d62d0eb348ed3ea6c34f5f4b7751e4c
Alan <yao.han1@northeastern.edu>
2023-01-26 17:30:20 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   reactStrictMode: true,
   images: {
     domains: [
       'lh3.googleusercontent.com',
       'cdn.pixabay.com',
       'p16-amd-va.tiktokcdn.com',
-      'image.shutterstock.com'
+      'image.shutterstock.com',
     ],
   },
 };
